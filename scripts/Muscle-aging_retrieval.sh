a2103ea1a3d0bf1af635e077427b6dab464dc5e7
KennyLee123 <49857756+KennyLee123@users.noreply.github.com>
2025-11-11 23:16:18 +0800

    Update Muscle-aging_retrieval.sh

--- scripts/Muscle-aging_retrieval.sh
+++ scripts/Muscle-aging_retrieval.sh
@@ -1,17 +1,17 @@
 for cross in "human_mouse" "mouse_human"; do
 IFS="_" read -r species1 species2 <<< "$cross"
 for method in "cellplm" "geneformer" "scgpt" "scimilarity" "scmulan" "uce" "scimilarity"; do
-python ../retrieval/main.py --input_adata "./raw_data/Muscle-aging/${species1}.h5ad" \
-    --paired_adata "./raw_data/Muscle-aging/${species2}.h5ad" \
-    --input_embeddings "./raw_data/Muscle-aging/embeddings/${species1}_clean_${method}.npy" \
-    --pair_embeddings "./raw_data/Muscle-aging/embeddings/${species2}_clean_${method}.npy" \
+python ../retrieval/main.py --input_adata "../data/Muscle-aging/${species1}.h5ad" \
+    --paired_adata "../data/Muscle-aging/${species2}.h5ad" \
+    --input_embeddings "../data/Muscle-aging/embeddings/${species1}_clean_${method}.npy" \
+    --pair_embeddings "../data/Muscle-aging/embeddings/${species2}_clean_${method}.npy" \
     --method ${method} \
     --retrieved_for_each_cell 100 \
     --faiss_search L2 \
     --norm no \
     --has_paired_data \
     --obs assay,cell_type \
-    --output_dir "./raw_data/Muscle-aging/${cross}/${method}"\
+    --output_dir "../data/Muscle-aging/${cross}/${method}"\
 
 done
 done
@@ -20,17 +20,17 @@ done
 for cross in "human_mouse" "mouse_human"; do
 IFS="_" read -r species1 species2 <<< "$cross"
 for method in "pca" "scvi" "linearscvi" "cellblast"; do
-python ../retrieval/main.py --input_adata "./raw_data/Muscle-aging/${species1}.h5ad" \
-    --paired_adata "./raw_data/Muscle-aging/${species2}.h5ad" \
-    --input_embeddings "./raw_data/Muscle-aging/embeddings/${cross}_${method}/${species1}_clean_${method}.npy" \
-    --pair_embeddings "./raw_data/Muscle-aging/embeddings/${cross}_${method}/${species2}_clean_${method}.npy" \
+python ../retrieval/main.py --input_adata "../data/Muscle-aging/${species1}.h5ad" \
+    --paired_adata "../data/Muscle-aging/${species2}.h5ad" \
+    --input_embeddings "../data/Muscle-aging/embeddings/${cross}_${method}/${species1}_clean_${method}.npy" \
+    --pair_embeddings "../data/Muscle-aging/embeddings/${cross}_${method}/${species2}_clean_${method}.npy" \
     --method ${method} \
     --retrieved_for_each_cell 100 \
     --faiss_search L2 \
     --norm no \
     --has_paired_data \
     --obs assay,cell_type \
-    --output_dir "./raw_data/Muscle-aging/${cross}/${method}"\
+    --output_dir "../data/Muscle-aging/${cross}/${method}"\
 
 done
 done
