d92e302a63b4337e277879a5b4f468dfedae9060
KennyLee123 <49857756+KennyLee123@users.noreply.github.com>
2025-11-11 23:07:01 +0800

    Update Arterial-Aortia_retrieval.sh

--- scripts/Arterial-Aortia_retrieval.sh
+++ scripts/Arterial-Aortia_retrieval.sh
@@ -1,14 +1,14 @@
 for method in "cellplm" "geneformer" "scgpt" "scimilarity" "scmulan" "uce" "scimilarity" "pca" "scvi" "linearscvi" "cellblast"; do
-python ../retrieval/main.py --input_adata "./raw_data/Arterial-Aortia/aorta_st.h5ad" \
-    --paired_adata "./raw_data/Arterial-Aortia/aorta_sc.h5ad" \
-    --input_embeddings "./raw_data/Arterial-Aortia/embeddings/aorta_st_${method}.npy" \
-    --pair_embeddings "./raw_data/Arterial-Aortia/embeddings/aorta_sc_${method}.npy" \
+python ../retrieval/main.py --input_adata "../data/Arterial-Aortia/aorta_st.h5ad" \
+    --paired_adata "../data/Arterial-Aortia/aorta_sc.h5ad" \
+    --input_embeddings "../data/Arterial-Aortia/embeddings/aorta_st_${method}.npy" \
+    --pair_embeddings "../data/Arterial-Aortia/embeddings/aorta_sc_${method}.npy" \
     --method ${method} \
     --retrieved_for_each_cell 100 \
     --faiss_search L2 \
     --norm no \
     --has_paired_data \
     --obs donor_id,cell_type \
-    --output_dir "./raw_data/Arterial-Aortia/st_sc/${method}"\
+    --output_dir "../data/Arterial-Aortia/st_sc/${method}"\
 
 done
